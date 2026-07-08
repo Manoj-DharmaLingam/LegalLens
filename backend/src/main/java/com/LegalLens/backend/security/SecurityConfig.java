13788927b95af7bcf04529474ae36a4100ea52f6
Manoj <dsmanoj1553@gmail.com>
2026-07-08 22:58:34 +0530

    the problem is with rbsc it is found and fixed

--- backend/src/main/java/com/LegalLens/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/LegalLens/backend/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -59,11 +58,6 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
         return authConfig.getAuthenticationManager();
-    }
-
-    @Bean
-    public static GrantedAuthorityDefaults grantedAuthorityDefaults() {
-        return new GrantedAuthorityDefaults(""); 
     }
 
 }

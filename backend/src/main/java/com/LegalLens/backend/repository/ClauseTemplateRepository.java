f78d25f29069475769ef7990276444d11de2a62c
ManojDS <dsmanoj1553@gmail.com>
2026-07-08 10:00:57 +0530

    Delete backend directory

--- backend/src/main/java/com/LegalLens/backend/repository/ClauseTemplateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.LegalLens.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.LegalLens.backend.model.ClauseTemplate;
-
-public interface ClauseTemplateRepository extends JpaRepository<ClauseTemplate, Long> {
-}
--- backend/src/main/java/com/LegalLens/backend/repository/ComplianceRuleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.LegalLens.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.LegalLens.backend.model.ComplianceRule;
-
-
-public interface ComplianceRuleRepository extends JpaRepository<ComplianceRule, Long> {
-}
